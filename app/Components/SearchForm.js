306c3ad8998839d8360f71472a832d9433d61daa
“郭辉” <guoh@lecuntao.com>
2017-04-14 15:58:58 +0800

    07.文本框上的事件

--- app/Components/SearchForm.js
+++ app/Components/SearchForm.js
@@ -35,11 +35,17 @@
             <TextInput
               style={{height:50}}
               placeholder="搜索 ..."
-              // clearButtonMode="unless-editing" //清除按钮 always , never ,  while-editing , unless-editing
-              clearTextOnFocus={true} //文本框进入焦点状态是否情况内容
+              clearButtonMode="while-editing" //清除按钮 always , never ,  while-editing , unless-editing
+              // clearTextOnFocus={true} //文本框进入焦点状态是否情况内容
               // enablesReturnKeyAutomatically={true} //没有内容的时候禁用回车键,有内容开启回车键
-              autoFocus={true}
+              // autoFocus={true}
               returnKeyType="search" // 回车键 go , join , next , send , search
+              onFocus={() => console.log('onFocus')} //文本框获得焦点
+              onBlur={() => console.log('onBlur')} //离开文本框
+              onChange={() => console.log('onChange')} // 文本框文字发送变化时
+              onChangeText={(text)=> console.log(text)} // 文本框文字发送变化时 区别可以把有变化的文字作为给他绑定函数的一个参数
+              onEndEditing={() => console.log('onEndEditing')} // 完成输入 的时候会执行
+              onSubmitEditing={() => console.log('onSubmitEditing')} // 文本框输入完按回车键执行
               // placeholderTextColor="#6435c9"
               // secureTextEntry // 密码
               // autoFocus //默认选中
